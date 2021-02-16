b7c01a6b4de38b8b1f7c6b1438e64659ad036c98
Mickael Lecoq <mickael@lecoq.me>
2021-02-16 14:49:54 +0100

    init project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
+import { View } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+const Tab1 = createMaterialTopTabNavigator();
+
+const Tab2 = createMaterialTopTabNavigator();
+
+const snapPoints = [0, 300, "50%"];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomSheetModalProvider>
+        <Tab1.Navigator initialRouteName="Tab 1">
+          <Tab1.Screen name="Tab 1">{() => <Text>Tab 1</Text>}</Tab1.Screen>
+          <Tab1.Screen name="Tab2">
+            {() => (
+              <Tab2.Navigator initialRouteName="Tab 2.1">
+                <Tab2.Screen name="Tab 2.1">
+                  {() => <Text>Tab 2.1</Text>}
+                </Tab2.Screen>
+
+                <Tab2.Screen name="Tab 2.2">
+                  {() => (
+                    <View>
+                      <Text>Tab 2.2</Text>
+                      <BottomSheetModal
+                        waitFor={[]}
+                        simultaneousHandlers={[]}
+                        snapPoints={snapPoints}
+                      >
+                        <Text>Modal</Text>
+                      </BottomSheetModal>
+                    </View>
+                  )}
+                </Tab2.Screen>
+              </Tab2.Navigator>
+            )}
+          </Tab1.Screen>
+        </Tab1.Navigator>
+      </BottomSheetModalProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
